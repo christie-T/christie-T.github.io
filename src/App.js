9e05f46bb918cdabb3fdb5efbb08763eee36e4b0
Christie <christietang23@gmail.com>
2024-04-05 01:45:52 -0400

    V2.0

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar"; // import component from where it is defined
+import { Banner } from "./components/Banner";
+import { Projects } from "./components/Projects";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,4 @@
+import {useState, useEffect} from "react"
+import email from '../assets/img/icons8-email-50.png';
+
+export const{}
